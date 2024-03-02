2568a520b903ebf35477628acb3355cf609b692f
Mykhailo20 <myhailo20189@gmail.com>
2024-03-02 16:13:23 +0200

    feat (lab2): implement the FuzzySet class

---
+++ lab2/main_project/utils/display_data.py
@@ -0,0 +1,16 @@
+def print_matrix(matrix, precision=3):
+    """
+    Prints a NumPy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The matrix to be printed.
+        precision (int): The number of decimal places to display (default is 2).
+    
+    Returns: None
+    """
+    for row in matrix:
+        print("[", end="")
+        row_el_no = len(row)
+        for i in range(row_el_no):
+            print(f"{row[i]:.{precision}f}", end=", " if i != row_el_no - 1 else "")
+        print("]")
---
+++ lab2/main_project/utils/fuzzy_set.py
@@ -0,0 +1,41 @@
+import numpy as np
+import display_data
+
+class FuzzySet:
+    def __init__(self, parameter_values: list|tuple, expert_evaluations: list|tuple):
+        self.parameter_values = parameter_values
+        self.n = len(self.parameter_values)
+        self.expert_evaluations = expert_evaluations
+        self.A = np.zeros(shape=[self.n, self.n], dtype='float32')
+        self.__calc_A__()
+        self.column_sums = self.A.sum(axis=0)
+        self.inverted_sums = 1.0 / self.column_sums
+        self.membership_function = self.inverted_sums / max(self.inverted_sums)
+        
+    def __calc_A__(self):
+        self.A[self.n - 1] = np.append(self.expert_evaluations, np.ones(1, dtype='float32'))
+        i = self.n - 2
+        while i >= 0:
+            k = i + 1
+            for j in range(self.n):
+                if i > j:
+                    self.A[i, j] = self.A[k, j] / self.A[k, i]
+                elif i < j:
+                    self.A[i, j] = 1.0 / self.A[j, i]
+                else:
+                    self.A[i, j] = 1.0
+            i -= 1
+
+
+if __name__ == "__main__":
+    parameter_values = np.array((170, 175, 180, 185, 190, 195))
+    expert_evaluations = np.array((9, 7, 5, 3, 1))
+    fuzzy_set = FuzzySet(parameter_values, expert_evaluations)
+
+    print(f"fuzzy_set.parameter_values = {fuzzy_set.parameter_values}")
+    print(f"fuzzy_set.expert_evaluations = {fuzzy_set.expert_evaluations}")
+    print('\nfuzzy_set.A:')
+    display_data.print_matrix(fuzzy_set.A)
+    print(f"\nfuzzy_set.column_sums = {fuzzy_set.column_sums}")
+    print(f"fuzzy_set.inverted_sums = {fuzzy_set.inverted_sums}")
+    print(f"fuzzy_set.membership_function = {fuzzy_set.membership_function}")
